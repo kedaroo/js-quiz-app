9f5db771810a636dbf954885bf25bf77d934c1ab
Kedar Basutkar <41827102+kedaroo@users.noreply.github.com>
2022-11-01 12:30:58 +0530

    fetch: questions from data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import data from "./data.txt";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    fetch(data)
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions));
+  }, []);
+
   return <div className="font-bold">Hello World</div>;
 }
 
